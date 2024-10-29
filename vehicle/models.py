8baa230f5c1efae215bea85c23803ef33ae1f508
Viktor Nikulaev <viktor_1611@mail.ru>
2024-10-29 19:27:40 +0300

    dz

---
+++ vehicle/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+class Car(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Машина'
+        verbose_name_plural = 'Машины'
+
+
+class Moto(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Мотоцикл'
+        verbose_name_plural = 'Мотоциклы'
+
+
+class Milage(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Машина', related_name='milage')
+    moto = models.ForeignKey(Moto, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Мотоцикл', related_name='milage')
+
+    milage = models.PositiveIntegerField(verbose_name='Пробег')
+    year = models.SmallIntegerField(verbose_name='Год регистрации')
+
+    def __str__(self):
+        return f'{self.moto if self.moto else self.car} - {self.year}'
+
+    class Meta:
+        verbose_name = 'Пробег'
+        verbose_name_plural = 'Пробег'
+        ordering = ('-year',)
